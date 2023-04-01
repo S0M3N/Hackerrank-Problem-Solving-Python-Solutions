a04fef006c999780dbb6b03c5befc49541806f3f
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-04-01 19:23:07 +0530

    making it more simple
    Instead of using for loop you can use the inbuilt sum function in python for the list that would give you the sum of all the elements then you can directly apply your logic. By the way great logic!

--- HackerRank-Mini-Max Sum/Mini-Max_Sum.py
+++ HackerRank-Mini-Max Sum/Mini-Max_Sum.py
@@ -8,10 +8,7 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    sum=0
-    for i in range(len(arr)):
-        sum+=arr[i]
-    print ( sum-max(arr), sum-min(arr))
+    print ( sum(arr)-max(arr), sum(arr)-min(arr))
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
